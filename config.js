3ad794896662611b8ba2698f67a5483c61ebbfef
a-kuei-cy <tea@sces.cy.edu.tw>
2026-08-07 13:46:34 +0800

    更新 config.js via EZPage

---
+++ config.js
@@ -0,0 +1,6 @@
+window.APP_CONFIG = {
+  API_URL: 'https://script.google.com/macros/s/AKfycbzazpqetxYuBFHePZ-FAb2Epqk3jyyTDSfw9lb6BOobv52pq17S4AkInPEb9TKnLoaW/exec',
+  SYSTEM_URL: 'management.html',
+  STUDENT_TASKS_URL: 'student-tasks.html',
+  DEMO_MODE: false
+};
